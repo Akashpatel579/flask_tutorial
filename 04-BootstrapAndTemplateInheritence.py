90ceb055815eec8c18672854b093871861fa44f2
Akash Patel <akashpatel0509@gmail.com>
2020-05-05 12:59:06 -0400

    04-BootstrapAndTemplateInheritence tutorial

---
+++ 04-BootstrapAndTemplateInheritence.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("04-index.html")
+
+@app.route("/contactus")
+def contactus():
+    return  render_template("04-contactus.html")
+
+
+if __name__ == "__main__":
+    app.run()
